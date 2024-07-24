64e513a66609adc03769f3747a68c47024ddf686
Gunnugupta <166784443+Gunnugupta@users.noreply.github.com>
2024-07-24 23:42:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -48,7 +48,7 @@ Tʜɪs Bᴏᴛ Aʟꜱᴏ Sᴜᴘᴘᴏʀᴛs Cᴜsᴛᴏᴍ Tʜᴜᴍʙɴᴀɪʟ
 
     ABOUT_TXT = """<b>╭───────────⍟
 • ᴍy ɴᴀᴍᴇ : {}
-• ᴜᴘᴅᴀᴛᴇꜱ : <a>href=https://t.me/moviesworldupdates>HaRsHiT</a>
+• ᴜᴘᴅᴀᴛᴇꜱ : <a href=https://t.me/moviesworldupdates>HaRsHiT</a>
 ╰───────────────⍟ """
 
     HELP_TXT = """
